cf03af38a5b65f7bc19ecb00504894c4d7d3d161
Soukainam <Soukainam@users.noreply.github.com>
2015-06-16 02:44:34 +0200

    update QrDAO suite pb merge de lorie

--- OsSim-master/edu/upc/fib/ossim/dao/QrDAO.java
+++ OsSim-master/edu/upc/fib/ossim/dao/QrDAO.java
@@ -1,78 +1,36 @@
-
-/*<<<<<<< HEAD
->>>>>>> 14b0a3fc4e10ee42898de8097d53aebf8b6f60d8
 package edu.upc.fib.ossim.dao;
 
 import java.util.List;
 
 import edu.upc.fib.ossim.mcq.model.Answer;
+import edu.upc.fib.ossim.mcq.model.Bid;
+import edu.upc.fib.ossim.mcq.model.ProcessusSimulationMemoire;
 import edu.upc.fib.ossim.mcq.model.ProcessusSimulationProcessus;
 import edu.upc.fib.ossim.mcq.model.QR;
-import edu.upc.fib.ossim.mcq.model.Simulation;
 import edu.upc.fib.ossim.mcq.model.SimulationMemoire;
 import edu.upc.fib.ossim.mcq.model.SimulationProcessus;
 
 public interface QrDAO {
-	  QR getInfoQR(int IdQR);
-	  List<Answer>getAnswersQR(int IdQR);
-	  List getProcQR(int idQR);
-	  List getBidByProc(int id_Proc);
-	  List getProcQRMem(int idQR);
-	  List<ProcessusSimulationProcessus> getProcQRProcArriving(int idQR);
-	  List<ProcessusSimulationProcessus> getProcQRProcReady(int idQR);
+	  QR getInfoQR(int IdQR); 
+	  List<Answer> getAnswersQR(int IdQR); 
 	  SimulationProcessus getParamQRProc(int idQR);
+	  List<ProcessusSimulationProcessus> getProcQRProcArriving(int idQR); //
+	  List<ProcessusSimulationProcessus> getProcQRProcReady(int idQR); //
+	  List<Bid> getBidByProc(int id_Proc);
+	  List<ProcessusSimulationMemoire> getProcQRMem(int idQR);	  
 	  SimulationMemoire getParamQRMem(int idQR);
+	  
 	  void creerQRProcessus(QR q);
 	  void creerQRMemoire(QR q);
-	  void creerReponseQr();
-	  void creerParamQrProcessus();
-	  void creerParamQrMemoire();
-	  void creerProcessusQr();
-	  void creerProcessusQrArriving();
-	  void creerProcessusQrReady();
-	  void creerMemoireProcessusQr();
-	  void creerBidMemoire();
+	  void creerReponseQr(List<Answer> listAns);
+	  void creerParamQrProcessus(SimulationProcessus simp);
+	  void creerParamQrMemoire(SimulationMemoire simm);	  
+	  void creerProcessusQrArriving(List<ProcessusSimulationProcessus> listroA);
+	  void creerProcessusQrReady(List<ProcessusSimulationProcessus> listroR);
+	  void creerMemoireProcessusQr(List<ProcessusSimulationMemoire> listpro);
+	  void creerBidMemoire(List<Bid> bid);
 	  
 	  QR findQR(int idQR);
+	  void createQR(QR qr);
 
 }
-<<<<<<< HEAD
-=======
-=======*/
-
-package edu.upc.fib.ossim.dao;
-
-import java.util.List;
-
-import edu.upc.fib.ossim.mcq.model.Answer;
-import edu.upc.fib.ossim.mcq.model.ProcessusSimulationProcessus;
-import edu.upc.fib.ossim.mcq.model.QR;
-import edu.upc.fib.ossim.mcq.model.Simulation;
-import edu.upc.fib.ossim.mcq.model.SimulationMemoire;
-import edu.upc.fib.ossim.mcq.model.SimulationProcessus;
-
-public interface QrDAO {
-	  QR getInfoQR(int IdQR);
-	  List<Answer>getAnswersQR(int IdQR);
-	  List getProcQR(int idQR);
-	  List getBidByProc(int id_Proc);
-	  List getProcQRMem(int idQR);
-	  List<ProcessusSimulationProcessus> getProcQRProcArriving(int idQR);
-	  List<ProcessusSimulationProcessus> getProcQRProcReady(int idQR);
-	  SimulationProcessus getParamQRProc(int idQR);
-	  SimulationMemoire getParamQRMem(int idQR);
-	  void creerQRProcessus(QR q);
-	  void creerQRMemoire(QR q);
-	  void creerReponseQr();
-	  void creerParamQrProcessus();
-	  void creerParamQrMemoire();
-	  void creerProcessusQr();
-	  void creerProcessusQrArriving();
-	  void creerProcessusQrReady();
-	  void creerMemoireProcessusQr();
-	  void creerBidMemoire();
-	  
-	  QR findQR(int idQR);
-
-}
-
